12195ef55bf881720f5c6372f30d8b1888738550
Nastiky92 <nastyaralchik@gmail.com>
2024-09-21 23:42:44 +0300

    Update js

--- script.js
+++ script.js
@@ -1,11 +1,5 @@
 const form = document.querySelector(".form");
-form.addEventListener("submit", (event) => {
-    // Предотвращает действие браузера по умолчанию. В данном случае — отправку формы
-    // https://learn.javascript.ru/default-browser-action
-    event.preventDefault();
-    console.log('Данные отправлены')
-
-});
+const sendButton = document.querySelector(".send-btn");
 
 const nameUser = document.querySelector('#name')
 const secondNameUser = document.querySelector('#secondName')
@@ -13,22 +7,29 @@ const emailUser = document.querySelector('#email')
 const phoneUser = document.querySelector('#phone')
 const agreeCheck = document.querySelector('#agree')
 
-async function preventDefault() {
+form.addEventListener("submit", (event) => {
+    // Предотвращает действие браузера по умолчанию. В данном случае — отправку формы
+    // https://learn.javascript.ru/default-browser-action
+    event.preventDefault();
+    console.log('Данные отправлены')
+
     fetch(`https://polinashneider.space/user`, {
-        method: 'POST',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer: codekozz'
-        },
-        body: JSON.stringify({
-            "name": nameUser.value,
-            "secondName": secondNameUser.value,
-            "phone": phoneUser.value,
-            "email": emailUser.value,
-            "agree": function check() {
-                return document.getElementById("myCheck").checked
-            }
-        }),
-    })
-}
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer: codekozz'
+            },
+            body: JSON.stringify({
+                "name": nameUser.value,
+                "secondName": secondNameUser.value,
+                "phone": phoneUser.value,
+                "email": emailUser.value,
+                "agree": true
+            }),
+
+        })
+        .then((result) => result.json())
+        .then((data) => console.log(data))
+
+});
