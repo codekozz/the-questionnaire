4380b129207206331f4fdbc179fa85d5baafbc38
Nastiky92 <nastyaralchik@gmail.com>
2024-09-21 23:12:06 +0300

    Update js

--- script.js
+++ script.js
@@ -1,8 +1,34 @@
 const form = document.querySelector(".form");
 form.addEventListener("submit", (event) => {
-  // Предотвращает действие браузера по умолчанию. В данном случае — отправку формы
-  // https://learn.javascript.ru/default-browser-action
-  event.preventDefault();
+    // Предотвращает действие браузера по умолчанию. В данном случае — отправку формы
+    // https://learn.javascript.ru/default-browser-action
+    event.preventDefault();
 
-  // Здесь твой код
+
 });
+
+const nameUser = document.querySelector('#name')
+const secondNameUser = document.querySelector('#secondName')
+const emailUser = document.querySelector('#email')
+const phoneUser = document.querySelector('#phone')
+const agreeCheck = document.querySelector('#agree')
+
+async function preventDefault() {
+    fetch(`https://polinashneider.space/user`, {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer: codekozz'
+        },
+        body: JSON.stringify({
+            "name": nameUser.value,
+            "secondName": secondNameUser.value,
+            "phone": phoneUser.value,
+            "email": emailUser.value,
+            "agree": function check() {
+                return document.getElementById("myCheck").checked
+            }
+        }),
+    })
+}
